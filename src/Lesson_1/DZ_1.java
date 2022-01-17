8723f28e54d125a33a868ff42e9b446cba385b75
VVasilievW <vitavas67@mail.ru>
2022-01-17 16:51:49 +0300

    3 exercise

--- src/Lesson_1/DZ_1.java
+++ src/Lesson_1/DZ_1.java
@@ -3,12 +3,21 @@ package Lesson_1;
 public class DZ_1 {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = 3, b = -7;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else{
+            System.out.println("Сумма отрицательная");
+        }
     }
 
 }
