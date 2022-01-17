e0189ba4ef133b14976ad5442302a6aa44b033a6
VVasilievW <vitavas67@mail.ru>
2022-01-17 17:23:58 +0300

    4 exercise

--- src/Lesson_1/DZ_1.java
+++ src/Lesson_1/DZ_1.java
@@ -4,6 +4,7 @@ public class DZ_1 {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
     }
 
     public static void printThreeWords() {
@@ -12,11 +13,23 @@ public class DZ_1 {
         System.out.println("Apple");
     }
     public static void checkSumSign() {
-        int a = 3, b = -7;
+        int a = 3, b = 4;
         if (a + b >= 0) {
             System.out.println("Сумма положительная");
         } else{
             System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 200;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
         }
     }
 
