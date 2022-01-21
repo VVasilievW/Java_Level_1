f5f9ff25e195db88b89233de5bece1af4026658c
VVasilievW <vitavas67@mail.ru>
2022-01-21 15:25:03 +0300

    1 exercise

---
+++ src/Lesson_2/DZ_2.java
@@ -0,0 +1,17 @@
+package Lesson_2;
+
+public class DZ_2 {
+    public static void main(String[] args) {
+
+        public static boolean checkSumm (int a, int b) {
+            int sum = a +b;
+            if (sum > 10 && sum <=20) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
+    }
+}
