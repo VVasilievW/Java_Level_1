b47cbe03b94e717ca8ac59846a777856f975017d
VVasilievW <vitavas67@mail.ru>
2022-01-26 19:45:16 +0300

    2 exercise

--- src/Lesson_3/DZ_3.java
+++ src/Lesson_3/DZ_3.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class DZ_3 {
     public static void main(String[] args) {
         invertMass();
+        createArray100();
     }
 
     //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
@@ -18,6 +19,16 @@ public class DZ_3 {
             else arr1[i] = 0;
         }
         System.out.println(Arrays.toString(arr1));
+
+    }
+    //2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+
+    public static void createArray100() {
+        int [] arr2 = new int[100];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = i +1;
+        }
+        System.out.println("2)\n" + Arrays.toString(arr2));
 
     }
 }
